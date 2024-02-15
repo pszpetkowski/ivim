cb18c9f21f43739a5e3a5a03c2c645059d2141f4
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2024-02-15 01:24:44 +0100

    Add formatter plugin definition based on conform.nvim

---
+++ lua/ivim/plugins/formatter.lua
@@ -0,0 +1,15 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	lazy = true,
+	opts = {
+		-- Define formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_format" },
+			rust = { "rustfmt" },
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	},
+}
