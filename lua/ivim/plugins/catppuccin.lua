50974ffda281237b71682775ce8c1607a84b4dc9
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2024-02-27 21:12:19 +0100

    Enable integrations and additional config for catppucin

--- lua/ivim/plugins/catppuccin.lua
+++ lua/ivim/plugins/catppuccin.lua
@@ -10,6 +10,25 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      integrations = {
+        cmp = true,
+        dashboard = true,
+        mason = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        neotree = true,
+        treesitter = true,
+        which_key = true,
+      },
+    })
     local color_scheme = core_utils.get_system_color_scheme()
     vim.cmd("colorscheme " .. themes[color_scheme])
   end,
