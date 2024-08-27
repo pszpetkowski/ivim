0090f384b111dbf89f75ad96f53cacadee0cd04b
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2024-08-28 01:40:06 +0200

    Rewrite whichkey mappings to the new format

--- lua/ivim/plugins/whichkey.lua
+++ lua/ivim/plugins/whichkey.lua
@@ -7,32 +7,29 @@ return {
   end,
   config = function()
     local wk = require("which-key")
-    wk.register({
-      [";"] = { "<cmd>Dashboard<cr>", "Dashboard" },
-      ["c"] = { "<cmd>bd<cr>", "Close Buffer" },
-      ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-      ["t"] = { "<cmd>ToggleTerm<cr>", "Terminal" },
-      ["l"] = {
-        name = "LSP",
-        a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-        d = { "<cmd>Mason<cr>", "Dashboard" },
-        i = { "<cmd>LspInfo<cr>", "Info" },
-        l = { "<cmd>lua vim.diagnostic.open_float()<cr>", "List Diagnostics" },
-        s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-        S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
-      },
-      ["p"] = {
-        name = "Plugins",
-        d = { "<cmd>Lazy<cr>", "Dashboard" },
-      },
-      ["s"] = {
-        name = "Search",
-        f = { "<cmd>Telescope find_files<cr>", "Find files" },
-        t = { "<cmd>Telescope live_grep<cr>", "Find text" },
-        T = { "<cmd>TodoTelescope<cr>", "Search TODOs" },
-      },
-      ["Q"] = { "<cmd>q!<cr>", "Quit (force)" },
-      ["q"] = { "<cmd>q<cr>", "Quit" },
-    }, { prefix = "<leader>" })
+    wk.add({
+      { "<leader>;", "<cmd>Dashboard<cr>", desc = "Dashboard" },
+      { "<leader>c", "<cmd>bd<cr>", desc = "Close Buffer" },
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+      { "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Terminal" },
+      { "<leader>l", group = "LSP" },
+      { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action" },
+      { "<leader>ld", "<cmd>Mason<cr>", desc = "Dashboard" },
+      { "<leader>li", "<cmd>LspInfo<cr>", desc = "Info" },
+      { "<leader>ll", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "List Diagnostics" },
+      { "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols" },
+      { "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols" },
+      { "<leader>p", group = "Plugins" },
+      { "<leader>pd", "<cmd>Lazy<cr>", desc = "Dashboard" },
+      { "<leader>s", group = "Search" },
+      { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>st", "<cmd>Telescope live_grep<cr>", desc = "Find text" },
+      { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "Search TODOs" },
+      {
+        mode = { "n", "v" },
+        { "<leader>Q", "<cmd>q!<cr>", desc = "Quit (force)" },
+        { "<leader>q", "<cmd>q<cr>", desc = "Quit" },
+      }
+    })
   end,
 }
